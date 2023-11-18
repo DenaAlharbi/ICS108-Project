22bc1437c4ae1cbf0621fb3bc80a800df9395bc1
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-11-18 20:45:32 +0300

    Add files via upload

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -4,8 +4,24 @@ import javafx.event.EventHandler;
 
 public class ButtonHandler extends parent implements EventHandler<ActionEvent> {
     public void handle(ActionEvent e){
-       // if(e.getSource() == changePicture){
-
-        //}
+        if (e.getSource() == addButton){
+            System.out.print("The add button was clicked");
+        }
+        else if (e.getSource() == deleteButton){
+            System.out.print("The delete button was clicked");
+        }
+        else if (e.getSource()==lookupButton) {
+            System.out.println("the lookup button was clicked");
+        }
+        else if (e.getSource()==changeStatus) {
+            System.out.println("the status button was clicked");
+        }
+        else if(e.getSource()==changePicture ){
+            //center.getChildren().remove();
+            System.out.println("the pic button was clicked");
+        }
+        else if (e.getSource()==addFriend) {
+            System.out.println("the add button was clicked");
+        }
     }
 }
